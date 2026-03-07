fe4aff20e46b307d4bba5b1aadc4d980614b46c4
nicole-dev-tech <nmakayi@byupathway.edu>
2026-03-07 23:32:45 +0200

    code1

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess?");
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higherr");
+            }
+            else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it, Congradulations!");
+            }
+        }
     }
 }
