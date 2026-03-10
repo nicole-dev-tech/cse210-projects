0b324dc716b9ae76a86160252968056098211122
nicole-dev-tech <nmakayi@byupathway.edu>
2026-03-10 21:06:17 +0200

    resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+       Job job1 = new Job();
+       job1._jobTitle = "Software developer";
+       job1._company = "Google";
+       job1._startYear = 2019;
+       job1._endYear = 2022;
+
+       Job job2 = new Job();
+       job2._jobTitle = "IT Auditor";
+       job2._company = "World Remit";
+       job2._startYear = 2022;
+       job2._endYear = 2023;
+
+       Resume myResume = new Resume();
+       myResume._name = "Nicole Makayi";
+
+       myResume._jobs.Add(job1);
+       myResume._jobs.Add(job2);
+
+       myResume.Display();
     }
 }
