440174e15e7762040658874e85d5262d4da556da
nicole-dev-tech <nmakayi@byupathway.edu>
2026-03-26 22:43:05 +0200

    order

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -8,7 +8,7 @@ class Program
 
         // Order 1 - USA Customer
         Address address1 = new Address("123 Maple Street", "Springfield", "IL", "USA");
-        Customer customer1 = new Customer("Emma Johnson", address1);
+        Customer customer1 = new Customer("Gift Scotch", address1);
 
         Order order1 = new Order(customer1);
         order1.AddProduct(new Product("Wireless Headphones", "WH-456", 89.99m, 1));
@@ -17,11 +17,11 @@ class Program
 
         // Order 2 - International Customer
         Address address2 = new Address("456 Ocean Avenue", "Toronto", "ON", "Canada");
-        Customer customer2 = new Customer("Liam Chen", address2);
+        Customer customer2 = new Customer("Jordan Kundai", address2);
 
         Order order2 = new Order(customer2);
         order2.AddProduct(new Product("Mechanical Keyboard", "MK-112", 129.99m, 1));
-        order2.AddProduct(new Product("Gaming Mouse", "GM-334", 45.00m, 1));
+        order2.AddProduct(new Product("Gaming", "GM-334", 45.00m, 1));
 
         // Display results for Order 1
         Console.WriteLine("=== ORDER 1 ===");
